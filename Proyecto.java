1743285739dda8d6b93d1cf333920c55513b604d
Pedrotote21 <pedropereyra139@gmail.com>
2025-10-29 18:55:24 -0300

    casi update

--- Proyecto.java
+++ Proyecto.java
@@ -1,34 +1,34 @@
 //Acceder a base de datos
-//import conector.SQLConnection;
+import conector.SQLConnection;
 import java.util.Scanner;
 //import conector.op;
 //Para printear
 import java.io.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public int Agregar() {
-    Scanner scanner = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
     System.out.print("Ingrese los datos del usuario,\nPrimero su nro de identificación: ");
     int nro_ide = sc.nextInt();
     System.out.print("Dirección del usuario(entre 35 caracteres): ");
-    String direccion = sc.next();
+    String direccion = sc.nextLine();
     System.out.print("Telefono(unico): ");
     int telefono = sc.nextInt();
-    SQLException e;
-   try (Connection conn = connect();
+    try (Connection conn = connect();
         PreparedStatement ps = conn.prepareStatement("INSERT INTO usuario (nro_ide,dirrecion,tel) VALUES (?,?,?)")) {
         ps.setInt(1, nro_ide);
         ps.setString(2, direccion);
         ps.setInt(3, telefono);
-        NoIngresado = false;
         ps.executeUpdate();
     } catch (SQLException e) {
         System.out.print("ERROR al ingresar.\n");
     }
-    if (e.isEmpty()){
-        System.out.println("Usuario agregado correctamente.");
-    }
-    scanner.close();
+    sc.close();
+    return 0;
 }
+
 
 public int Eliminar() {
     Scanner scanner = new Scanner(System.in);
@@ -72,7 +72,6 @@ public int Reclamos() {
     }
 
 
-
     private static void actualizarEmpleado(Scanner sc) {
         System.out.print("ID del empleado a actualizar: ");
         int id = sc.nextInt();
@@ -107,7 +106,7 @@ public static int verificar(String ing){
         return -1;
     }else{
         if (ing.equals("agregar")||ing.equals("1")){
-            return 1;
+            return Agregar();
         }else{
             if(ing.equals("eliminar")||ing.equals("2")){
                 return 0;
