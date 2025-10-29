eedf3a5b6559d37b56e912492ed04955923579cc
AgueroDaniel <danielaguee89@gmail.com>
2025-10-29 16:55:15 -0300

    Cambio de estructura y empese el proyecto.java

--- Proyecto.java
+++ Proyecto.java
@@ -1,33 +1,39 @@
 //Acceder a base de datos
 import conector.SQLConnection;
+import java.util.Scanner;
+//import conector.op;
 //Para printear
 import java.io.*;
 
-private int verificar(String ing){
-    if (ing.equals.("salir")){
+  public static int verificar(String ing){
+    ing=ing.toLowerCase();
+    if (ing.equals("salir")||ing.equals("exit")||ing.equals("4")){
         return -1;
     }else{
-        if (ing.equals.("agregar")){
+        if (ing.equals("agregar")||ing.equals("1")){
             return 1;
         }else{
-            if(ing.equals.("eliminar")){
+            if(ing.equals("eliminar")||ing.equals("2")){
                 return 0;
             }else{
-                if(ing.equals.("reclamos")){}
+                if(ing.equals("reclamos")||ing.equals("3")){
+                    return 0;
+                }
             }
         }
     }
     return 0;
 }
 
-public static void main(String args[]){
-    int selection = 0;
-    Scanner scanner = new Scanner(System.in);
-    String ingreso;
+ public static void main(String[] args){
+        int selection = 0;
+        Scanner scanner = new Scanner(System.in);
+    
     while (selection > -1){
-        System.out.println("\nQue desea realizar.\n1) Agregar un usuario(agregar).\n2) Eliminar un reclamo(eliminar).\n3) Listar reclamos:");
+        System.out.println("\nQue desea realizar:\n    1) Agregar un usuario(agregar).\n"+
+                        "    2) Eliminar un reclamo(eliminar).\n    3) Listar reclamos(listar)\n    4) Salir:");
         String ingreso = scanner.nextLine();
-        selection=verificar(ingreso.toLowerCase());
+        selection = verificar(ingreso);
     }
-
+    scanner.close();
 }
